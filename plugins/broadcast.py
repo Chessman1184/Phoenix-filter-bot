81e70ec53192b9b508d8b32a77892aba1ce3aa62
Chessman1184 <anjoriniyanuoluwa08@gmail.com>
2025-06-14 11:59:22 +0100

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,107 +1,82 @@
-from pyrogram import Client, filters
-import datetime
-import time
-from database.users_chats_db import db
-from info import ADMINS
-from utils import users_broadcast, groups_broadcast, temp, get_readable_time
 import asyncio
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup 
+import logging
+from datetime import datetime
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from utils import users_broadcast, groups_broadcast, temp, get_readable_time
+from database.users_chats_db import db
+from info import ADMINS, LOG_CHANNEL
 
-lock = asyncio.Lock()
+logger = logging.getLogger(__name__)
 
-@Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
-async def broadcast_cancel(bot, query):
-    _, ident = query.data.split("#")
-    if ident == 'users':
-        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ᴜsᴇʀs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
-        temp.USERS_CANCEL = True
-    elif ident == 'groups':
-        temp.GROUPS_CANCEL = True
-        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ɢʀᴏᴜᴘs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
-       
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_users(bot, message):
-    if lock.locked():
-        return await message.reply('Currently broadcast processing, Wait for complete.')
+async def broadcast_handler(bot: Client, message: Message):
+    """
+    Broadcast messages to all users or groups
+    """
+    try:
+        # Get all users
+        all_users = await db.get_all_users()
+        broadcast_msg = message.reply_to_message
+        
+        if len(message.command) > 1:
+            if message.command[1] == 'pin':
+                pin = True
+            else:
+                pin = False
+        else:
+            pin = False
+            
+        # Start broadcast
+        broadcast_start_time = datetime.now()
+        await message.reply_text("<b>Broadcasting messages... This may take a while.</b>")
+        
+        success_msg = await users_broadcast(bot, broadcast_msg, all_users, pin)
+        
+        time_taken = get_readable_time(
+            (datetime.now() - broadcast_start_time).seconds
+        )
+        
+        await message.reply_text(
+            f"<b>{success_msg}\nTime taken: {time_taken}</b>"
+        )
+        
+    except Exception as e:
+        logger.error(f"Broadcast error: {e}", exc_info=True)
+        await message.reply_text(f"<b>Error in broadcast:</b>\n<code>{str(e)}</code>")
 
-    p = await message.reply('<b>Do you want pin this message in users?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
-    msg = await bot.listen(chat_id=message.chat.id, user_id=message.from_user.id)
-    if msg.text == 'Yes':
-        is_pin = True
-    elif msg.text == 'No':
-        is_pin = False
-    else:
-        await p.delete()
-        return await message.reply_text('Wrong Response!')
-    await p.delete()
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ᴜsᴇʀs ⌛️</b>')
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    blocked = 0
-    deleted = 0
-    failed = 0
-    success = 0
-
-    async with lock:
-        async for user in users:
-            time_taken = get_readable_time(time.time()-start_time)
-            if temp.USERS_CANCEL:
-                temp.USERS_CANCEL = False
-                await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
-                return
-            sts = await users_broadcast(int(user['id']), b_msg, is_pin)
-            if sts == 'Success':
-                success += 1
-            elif sts == 'Error':
-                failed += 1
-            done += 1
-            if not done % 20:
-                btn = [[
-                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
-                ]]
-                await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
-        await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
-
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_group(bot, message):
-    p = await message.reply('<b>Do you want pin this message in groups?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
-    msg = await bot.listen(chat_id=message.chat.id, user_id=message.from_user.id)
-    if msg.text == 'Yes':
-        is_pin = True
-    elif msg.text == 'No':
-        is_pin = False
-    else:
-        await p.delete()
-        return await message.reply_text('Wrong Response!')
-    await p.delete()
-    chats = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ɢʀᴏᴜᴘs ⏳</b>')
-    start_time = time.time()
-    total_chats = await db.total_chat_count()
-    done = 0
-    failed = 0
-    success = 0
-    
-    async with lock:
-        async for chat in chats:
-            time_taken = get_readable_time(time.time()-start_time)
-            if temp.GROUPS_CANCEL:
-                temp.GROUPS_CANCEL = False
-                await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
-                return
-            sts = await groups_broadcast(int(chat['id']), b_msg, is_pin)
-            if sts == 'Success':
-                success += 1
-            elif sts == 'Error':
-                failed += 1
-            done += 1
-            if not done % 20:
-                btn = [[
-                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
-                ]]
-                await b_sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
-        await b_sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+@Client.on_message(filters.command("group_broadcast") & filters.user(ADMINS) & filters.reply)
+async def group_broadcast_handler(bot: Client, message: Message):
+    """
+    Broadcast messages to all groups
+    """
+    try:
+        # Get all groups
+        all_groups = await db.get_all_chats()
+        broadcast_msg = message.reply_to_message
+        
+        if len(message.command) > 1:
+            if message.command[1] == 'pin':
+                pin = True
+            else:
+                pin = False
+        else:
+            pin = False
+            
+        # Start broadcast
+        broadcast_start_time = datetime.now()
+        await message.reply_text("<b>Broadcasting to groups... This may take a while.</b>")
+        
+        success_msg = await groups_broadcast(bot, broadcast_msg, all_groups, pin)
+        
+        time_taken = get_readable_time(
+            (datetime.now() - broadcast_start_time).seconds
+        )
+        
+        await message.reply_text(
+            f"<b>{success_msg}\nTime taken: {time_taken}</b>"
+        )
+        
+    except Exception as e:
+        logger.error(f"Group broadcast error: {e}", exc_info=True)
+        await message.reply_text(f"<b>Error in group broadcast:</b>\n<code>{str(e)}</code>")
